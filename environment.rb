db11e5e836fc763613a5bb8750d0521034c93138
Sahil Agarwal <sahil.agarwal@xoom.com>
2016-10-03 15:47:39 -0700

    heroku vars

---
+++ environment.rb
@@ -0,0 +1,4 @@
+ENV['consumer_key1'] = 'LZoH721JuSjJHT23RHAcBZraL'
+ENV['consumer_key2'] = 'loZhormENc4dTGpz14FhkLhsJ8cD1Nhm5NIJvhfGJW8CBtxUis'
+ENV['access_token1'] = '21342397-QnSfELDDGocW8wk9vjvmD9bbS14xBBG11CLzsq1BA'
+ENV['access_token2'] = 'DGCks13XJEwv9l4SgnmblLPorPXIjEy3WCV50eBKvlQNt'
