7f4e259a7b45878becec2d50094a80a23812b000
Sahil Agarwal <sahildagarwal@gmail.com>
2018-10-04 14:00:13 -0700

    Add error handling for closed Parklab

--- lib/parklabDataPull.rb
+++ lib/parklabDataPull.rb
@@ -23,7 +23,7 @@ class ParklabDataPull < DataPull
     begin
       @date = Date.parse(@post.scan(/^\D+[\s\d,]+/)[0].strip)
       if today.yday == @date.yday && today.year == @date.year
-        @trucks = @post.scan(/Lunch.*/)[0][6..-1].split(",").map { |truck| truck.sub(/and/,'').strip }        
+        @trucks = @post.scan(/Lunch.*/)[0][6..-1].split(",").map { |truck| truck.sub(/and/,'').strip }
       end
     rescue
       return "Too lazy to find out, look here yourself (honestly though, it's probably the case that *they* actually have been too lazy to update their menu online): http://parklabjunction.com/ or https://lmgtfy.com/?q=parklab+junction."
@@ -45,8 +45,8 @@ class ParklabDataPull < DataPull
   def get_trucks
     parse_feed
     data = parse_post
-    if data == 'Too lazy to find out, look here yourself (honestly though, it\'s probably the case that *they* actually have been too lazy to update their menu online): http://parklabjunction.com/ or https://lmgtfy.com/?q=parklab+junction.'
-      return data
+    if data == 'Too lazy to find out, look here yourself (honestly though, it\'s probably the case that *they* actually have been too lazy to update their menu online): http://parklabjunction.com/ or https://lmgtfy.com/?q=parklab+junction.' || data == nil
+      return data || 'No menu\'s posted - see the latest here: https://www.facebook.com/ParklabJunction'
     end
     enhance_data
   end
--- test/test.rb
+++ test/test.rb
@@ -1,9 +1,9 @@
 require_relative '../lib/truckStopDataPull.rb'
 require_relative '../lib/parklabDataPull.rb'
 
-ts = TruckStopDataPull.new
-p ts.parse_feed
-p ts.parse_post
+# ts = TruckStopDataPull.new
+# p ts.parse_feed
+# p ts.parse_post
 
 pl = ParklabDataPull.new
 p pl.parse_feed
